faa64d38854753a4c0ae19f001468f24533c8c3e
pcp-cmd <Qwe2318738496@outlook.com>
2025-03-29 10:34:35 +0800

    Site updated: 2025-03-29 10:34:35

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/27/小组作业/","2025/03/28/我对写作的看法/"];function toRandomPost(){
+var posts=["2025/03/27/小组作业/","2025/03/28/我对写作的看法/","2025/03/27/theory/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
