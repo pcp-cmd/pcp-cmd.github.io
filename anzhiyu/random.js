a3f9d5364f3b761ba52fe98a9a08b6e3000192ec
pcp-cmd <Qwe2318738496@outlook.com>
2025-03-30 15:58:18 +0800

    Site updated: 2025-03-30 15:58:18

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/27/小组作业/","2025/03/28/我对写作的看法/","2025/03/27/theory/"];function toRandomPost(){
+var posts=["2025/03/27/theory/","2025/03/27/小组作业/","2025/03/28/我对写作的看法/","2025/03/30/英语写作学习思考/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
